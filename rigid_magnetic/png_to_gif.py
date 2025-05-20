85e170dfcb00e579ec023b8fefd99d4974c971d6
Carina Karner <oljaoctagon@github.com>
2025-05-20 11:12:55 +0200

    CHANGES: adapted gifmaker for magnetic purpose; added filelist writeout for html grid generation in ML Tools

--- rigid_magnetic/png_to_gif.py
+++ rigid_magnetic/png_to_gif.py
@@ -1,12 +1,19 @@
 import imageio
 import glob
 import numpy as np
+import argparse 
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-f", type=str, default="mag2p_shift_0_lambda_6_phi2d_0.0106_rid_1")
+args = parser.parse_args()
+
 images = []
-frames= glob.glob("*png")
-frame_values = np.sort(
-[ int(frame.split("_")[-1].split(".")[0]) for frame in frames])
-for i in frame_values:
-    filename="frame_{}.png".format(i)
+frames= glob.glob("*{}*.png".format(args.f))
+frame_values = np.sort([ int(frame.split("_")[1].split(".")[0]) for frame in frames])
+
+for frame_i in frame_values:
+    filename = "frame_{}_{}.png".format(frame_i,args.f)
     images.append(imageio.imread(filename))
 
-imageio.mimsave('./movie.gif', images, duration=0.2)
+imageio.mimsave('./{}_movie.gif'.format(args.f), images, duration=0.2)
